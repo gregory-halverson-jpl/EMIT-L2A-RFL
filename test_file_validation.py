3859b6e4d9554f70f19c21aa05e9906f1b166d02
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-12-05 15:00:35 -0800

    debugging NetCDF file read

--- test_file_validation.py
+++ test_file_validation.py
@@ -2,7 +2,7 @@ from os.path import expanduser
 
 from EMITL2ARFL import validate_NetCDF_file
 
-filename = "~/data/EMIT_L2A_RFL_001_20241006T165200_2428011_004.nc"
+filename = "~/data/EMIT_L2A_RFL_001_20230129T004447_2302816_003.nc"
 
 try:
     validate_NetCDF_file(filename)
