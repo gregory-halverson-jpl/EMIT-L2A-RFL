eb84af52de94d76afe929b103e8a982f482db06e
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-12-08 08:35:32 -0800

    testing file validation on file downloaded on HPC machine

--- test_file_validation.py
+++ test_file_validation.py
@@ -1,8 +1,13 @@
+import sys
 from os.path import expanduser
 
 from EMITL2ARFL import validate_NetCDF_file
 
-filename = "~/data/EMIT_L2A_RFL_001_20230129T004447_2302816_003.nc"
+if len(sys.argv) < 2:
+    print("Usage: python test_file_validation.py <netcdf_file>")
+    sys.exit(1)
+
+filename = sys.argv[1]
 
 try:
     validate_NetCDF_file(filename)
